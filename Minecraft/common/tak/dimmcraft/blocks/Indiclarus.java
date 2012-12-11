5997cb7ba9fd5b824f6ae002c1d78b6326d25ddf
Tak Suyu <taksuyu@gmail.com>
2012-12-11 10:14:57 -0800

    added Textures for Indiclarus and Slimecube
    see-through rendering on indiclarus to reflect glass like texture

--- Minecraft/common/tak/dimmcraft/blocks/Indiclarus.java
+++ Minecraft/common/tak/dimmcraft/blocks/Indiclarus.java
@@ -7,23 +7,25 @@ import tak.dimmcraft.conf.ConfIndiclarus;
 import tak.dimmcraft.conf.ConfPackageItems;
 import tak.dimmcraft.handlers.BlockHandler;
 import net.minecraft.src.Block;
+import net.minecraft.src.BlockBreakable;
 import net.minecraft.src.CreativeTabs;
 import net.minecraft.src.Material;
 
-public class Indiclarus extends BlockHandler {
+public class Indiclarus extends BlockBreakable {
 
 //	Loads the block against BlockLoader
 	public static void loadBlock (FMLInitializationEvent event) {
 		
-		final Block indiclarus = new Indiclarus(ConfIndiclarus.Indiclarus, 3, Material.glass)
+		final Block indiclarus = new Indiclarus(ConfIndiclarus.Indiclarus, 48, Material.glass, false)
 		.setHardness(-1F).setStepSound(Block.soundGlassFootstep)
 		.setBlockName("indiclarus").setCreativeTab(CreativeTabs.tabDecorations);
 		
 		BlockHandler.registerBlock(indiclarus, "Indiclarus", "pickaxe", 0);
 	}
 	
-	public Indiclarus(int par1, int par2, Material par3Material) {
-		super(par1, par2, par3Material);
+	public Indiclarus(int par1, int par2, Material par3Material, boolean par4) {
+	        
+		super(par1, par2, par3Material, par4);
 	}
 	
 	@Override
